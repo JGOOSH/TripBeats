2c44807b97c542ab8a90b5a16f394d160f5c4683
Hojae Jung <ghwo8844@gmail.com>
2018-07-14 13:27:59 -0500

    Spotify integrated

--- tripbeats/src/flask_api.py
+++ tripbeats/src/flask_api.py
@@ -2,9 +2,80 @@ from flask import Flask, request, Response, jsonify
 import json
 import requests
 import smtplib
+import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from flask_cors import CORS
+
+def get_response(destination):
+
+    URL = "https://api.spotify.com/v1/search"
+
+    PARAMS = {
+
+        "q": "The Sound of " + destination,
+
+        "type": "playlist"
+
+    }
+
+    HEADERS = {
+
+        "Accept": "application/json",
+
+        "Content-Type": "application/json",
+
+        "Authorization": "Bearer " + get_auth_token()
+
+    }
+
+    return requests.get(url=URL, params=PARAMS, headers=HEADERS).content
+
+def get_playlist(destination):
+
+    response = get_response(destination)
+
+    parsed = json.loads(response)["playlists"]
+
+    target_name = "The Sound of " + destination
+
+    if parsed["total"] > 0:
+
+        for item in parsed["items"]:
+
+            if target_name in item["name"]:
+
+                return item["external_urls"]["spotify"]
+
+        return parsed["items"][0]["external_urls"]["spotify"]
+
+    else:
+
+        return "No playlists found"
+
+def get_auth_token():
+
+    # For Phase 1:
+
+    URL = "https://accounts.spotify.com/api/token"
+
+    DATA = {
+
+        "grant_type": "client_credentials"
+
+    }
+
+    HEADERS = {
+
+        "Authorization": "Basic " + base64.b64encode("7a0ec994dec14b5aae6b994e6728d8ed:245e48245ec34e099e3da331465bc753".encode('utf-8'))
+
+    }
+
+    response = requests.post(url=URL, data=DATA, headers=HEADERS)
+
+    parsed = json.loads(response.text)
+
+    return parsed["access_token"]
 
 app = Flask(__name__)
 CORS(app)
@@ -40,7 +111,7 @@ def get_request():
 	server.quit()
 
 	data = {
-		'spotify' : 'LINK'
+		'spotify' : get_playlist(dest)
 	}
 
 	resp = jsonify(data)
