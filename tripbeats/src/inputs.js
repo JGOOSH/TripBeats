96387aff569386ca642205709d022ccc44bbc884
Jamin Goo <jamin_goo@eogresources.com>
2018-07-14 09:53:15 -0500

    rows unique id/key

--- tripbeats/src/inputs.js
+++ tripbeats/src/inputs.js
@@ -18,10 +18,54 @@ const styles = theme => ({
   }
 });
 
+const fetchInputData = async ({ querystring }) => {
+  const request = await fetch("/getthething", {
+    method: "POST",
+    headers: JSON.stringify(querystring)
+  });
+};
+
+const renderUserInputs = (classes, length) => {
+  return (
+    <form
+      className={classes.container}
+      noValidate
+      autoComplete="off"
+      key={`user-${length}`}
+    >
+      <TextField
+        id={`first_name-${length}`}
+        label="First Name"
+        className={classes.textField}
+        margin="normal"
+      />
+      <TextField
+        id={`last_name-${length}`}
+        label="Last Name"
+        className={classes.textField}
+        margin="normal"
+      />
+      <TextField
+        id={`email-${length}`}
+        label="Email"
+        className={classes.textField}
+        margin="normal"
+      />
+    </form>
+  );
+};
+
 class InputFields extends Component {
   state = {
-    userCount: 1
+    userInputList: []
   };
+
+  constructor(props) {
+    super(props);
+    const { classes } = this.props;
+    const { userInputList } = this.state;
+    userInputList.push(renderUserInputs(classes, userInputList.length));
+  }
 
   render() {
     const { classes } = this.props;
@@ -47,28 +91,17 @@ class InputFields extends Component {
             margin="normal"
           />
         </form>
-        <form className={classes.container} noValidate autoComplete="off">
-          <TextField
-            id="first_name"
-            label="First Name"
-            className={classes.textField}
-            margin="normal"
-          />
-          <TextField
-            id="last_name"
-            label="Last Name"
-            className={classes.textField}
-            margin="normal"
-          />
-          <TextField
-            id="email"
-            label="Email"
-            className={classes.textField}
-            margin="normal"
-          />
-        </form>
+        {this.state.userInputList.map(list => list)}
         <div className={classes.container}>
-          <Button variant="contained" className={classes.button}>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={() => {
+              let list = this.state.userInputList;
+              list.push(renderUserInputs(classes, list.length));
+              this.setState({ userInputList: list });
+            }}
+          >
             Add user
           </Button>
           <Button variant="contained" className={classes.button}>
