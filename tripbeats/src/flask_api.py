58565bf7ff2f9eb49cc27a2ebd5b6d0f7567dd81
Hojae Jung <ghwo8844@gmail.com>
2018-07-14 10:44:28 -0500

    fixed messaging

--- tripbeats/src/flask_api.py
+++ tripbeats/src/flask_api.py
@@ -3,8 +3,8 @@ from flask import request
 import json
 import requests
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
@@ -12,18 +12,10 @@ app = Flask(__name__)
 def get_request():
 	body = request.get_json()
 
-	dest = body['dest']
+	dest = body['trip']['destination']
 
 	fromaddr = 'no-reply@tripbeats.com'
 
-	msg = MIMEMultipart()
-	msg['From'] = fromaddr
-	msg['To'] = body['email']
-	msg['Subject'] = 'Your Trip to ' + dest
-
-	body = "YOUR MESSAGE HERE"
-	msg.attach(MIMEText(body, 'plain'))
-	 
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.starttls()
 
@@ -31,7 +23,38 @@ def get_request():
 	    data = json.load(f)
 
 	server.login(data['id'], data['passwd'])
-	text = msg.as_string()
-	server.sendmail(fromaddr, body['email'], text)
+
+	for person in body['trip']['people']:
+		msg = MIMEMultipart()
+		msg['From'] = fromaddr
+		msg['To'] = person['email']
+		msg['Subject'] = 'Your Trip to ' + dest
+
+		body = "YOUR MESSAGE HERE"
+		msg.attach(MIMEText(body, 'plain'))
+		text = msg.as_string()
+		server.sendmail(fromaddr, person['email'], text)
+
 	server.quit()
 
+
+"""
+server = smtplib.SMTP('smtp.googlemail.com', 25)
+server.starttls()
+
+with open('email_login.json') as f:
+    data = json.load(f)
+
+server.login(data['id'], data['passwd'])
+msg = MIMEMultipart()
+msg['From'] = 'no-reply@tripbeats.com'
+msg['To'] = 'ghwo8844@gmail.com'
+msg['Subject'] = 'Your Trip to'
+
+body = "YOUR MESSAGE HERE"
+msg.attach(MIMEText(body, 'plain'))
+text = msg.as_string()
+server.sendmail('no-reply@tripbeats.com', 'ghwo8844@gmail.com', text)
+
+server.quit()
+"""
